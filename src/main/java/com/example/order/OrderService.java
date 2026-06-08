65c6969cd417a54038fcb69e2ecda5d28c6ac80b
DVSychev <dvsychev@t1-team.ru>
2026-06-08 13:51:35 +0300

    refactor OrderService

--- src/main/java/com/example/order/OrderService.java
+++ src/main/java/com/example/order/OrderService.java
@@ -3,24 +3,55 @@ package com.example.order;
 import java.util.List;
 
 public class OrderService {
+
+    private static final String CLIENT_TYPE_VIP = "VIP";
+    private static final String CLIENT_TYPE_NEW = "NEW";
+    private static final double DISCOUNT_VIP = 0.9;
+    private static final double DISCOUNT_NEW = 0.95;
+    private static final int LARGE_SUM_ORDER = 1000;
+    private static final int DISCOUNT_LARGE_SUM_ORDER = 50;
+
+    /**
+     * Рассчитывает итоговую стоимость заказа с учетом скидок по типу клиента
+     * и общей сумме корзины.
+     *
+     * @param items список товаров в заказе, содержащих цену и количество
+     * @param type  тип клиента для определения персональной скидки ("VIP", "NEW" и др.)
+     * @return итоговая стоимость заказа после применения всех скидок
+     */
     public double calc(List<Item> items, String type) {
-        double s = 0;
+        if (items.isEmpty() || items == null) {
+            return 0.0;
+        }
+        double totalSum = totaAmountAllItems(items);
+        totalSum = discountDependingTypeCustomer(totalSum, type);
+        totalSum = discountDependingOrderAmount(totalSum);
+        return totalSum;
+    }
+
+    private double totaAmountAllItems(List<Item> items) {
+        double totalSum = 0;
         for (Item i : items) {
-            s += i.getPrice() * i.getQuantity();
+            totalSum += i.getPrice() * i.getQuantity();
+        }
+        return totalSum;
+    }
+
+    private double discountDependingTypeCustomer(double totalSum, String type) {
+        if (type.equals(CLIENT_TYPE_VIP)) {
+            totalSum = totalSum * DISCOUNT_VIP;
         }
 
-        if (type.equals("VIP")) {
-            s = s * 0.9;
+        if (type.equals(CLIENT_TYPE_NEW)) {
+            totalSum = totalSum * DISCOUNT_NEW;
         }
+        return totalSum;
+    }
 
-        if (type.equals("NEW")) {
-            s = s * 0.95;
+    private double discountDependingOrderAmount(double totalSum) {
+        if (totalSum > LARGE_SUM_ORDER) {
+            totalSum = totalSum - DISCOUNT_LARGE_SUM_ORDER;
         }
-
-        if (s > 1000) {
-            s = s - 50;
-        }
-
-        return s;
+        return totalSum;
     }
 }
